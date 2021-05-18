323c3546aae14fb55c09cd6abc1235381476873b
Neeraj Kheria <neerajkheria@Neerajs-Mac-mini.local>
2021-05-18 15:55:25 +0530

    featured commit

--- Sample.java
+++ Sample.java
@@ -1,9 +1,13 @@
 class Sample {
 	public static void main(String[] args){
 
-		for(int i=1; i<=10;i++){
+		for(int i=1; i<=3;i++){
 
-			System.out.println(i);
+			for(int j=1;j<=3;j++){
+
+			System.out.println(i+j);
+		}
+		
 		}
 
 	}
